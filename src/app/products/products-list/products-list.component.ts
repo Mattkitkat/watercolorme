6dd2ee7102f998ed5f9f0d63fad62ed5501a9daf
Matthew Cutajar <MatthewCutajar83@gmail.com>
2020-09-29 15:04:18 +0200

    Included Bulma and converted out the use of the NgAngularModal to a more simplistic version by using Bulma and a local component

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { MatDialog } from '@angular/material/dialog';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-products-list',
@@ -10,24 +11,14 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
 })
 export class ProductsListComponent implements OnInit {
   @Input() products: Product[];
-  lastClickedProduct: Product;
-  closeResult = '';
+  eventsSubject: Subject<Product> = new Subject<Product>();
   
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
-  productOnClick(product: Product, content){
-    this.lastClickedProduct = product;
-    this.open();
-  }
-
-  open() {
-    const dialogRef = this.dialog.open(ProductDetailComponent);
-    dialogRef.componentInstance.product = this.lastClickedProduct;
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
+  productOnClick(product: Product){
+    this.eventsSubject.next(product);
   }
 }
